281832f673cf4ff83f9203a4f69ba8d4e67fed75
elena-kovaleva <kovaleva2004elena@gmail.com>
2021-10-12 22:07:28 +0300

    task for SDT. Kovaleva114301

---
+++ Задание по лекции для Марины Михайловны)/Задание по лекции для Марины Михайловны)/Source.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	cout << "Здравствуйте, Марина Михайловна, извините, что так поздно)"<<endl;
+	system("pause");
+	return 0;
+}
